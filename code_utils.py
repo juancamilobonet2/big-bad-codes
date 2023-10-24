48e970e4c6dfc2b1caeed5fe7bb8709f8c3fca6a
juancamilobonet02 <j.bonet@uniandes.edu.co>
2023-10-23 23:33:15 -0500

    mas o menos funciona

--- code_utils.py
+++ code_utils.py
@@ -28,8 +28,57 @@ def multiply_matrices(matrix1, matrix2):
 def find_syndrome(parity_check_matrix, vector):
     return (multiply_matrices(parity_check_matrix, vector.transpose()))
 
+def random_permutation_matrix(n):
+    """
+    Returns a random permutation matrix of size n
+    """
+    permutation = [i for i in range(1,n+1)]
+    permutation = np.random.permutation(permutation)
+    permutation_matrix = np.zeros((n,n),dtype=int)
+    for i in range(n):
+        permutation_matrix[i,permutation[i]-1] = 1
+    return permutation_matrix
+
+
+def gaussian_elimination(matrix, start_column=0):
+    """
+    Gaussian elimination modulo 2
+    """
+    m, n = matrix.shape
+    # U son las transformaciones que se le hacen a la matriz
+    U = []
+    for i in range(m):
+        column = i + start_column
+        # find row with 1 in the ith place
+        for j in range(i,m):
+            if matrix[j,column] == 1:
+                matrix[[i,j]] = matrix[[j,i]]
+                transform = np.identity(m, dtype=int)
+                transform[[i,j]] = transform[[j,i]]
+                U.append(transform)
+                break
+        for j in range(m):
+            if matrix[j,column] == 1 and j != i:
+                matrix[j] = add_matrices(matrix[j], matrix[i])
+                transform = np.identity(m, dtype=int)
+                transform[j] = add_matrices(transform[j], transform[i])
+                U.append(transform)
+    return (U,matrix)
+
+def apply_transforms(U, matrix):
+    """
+    Applies the transformations in U to matrix
+    """
+    for i in range(len(U)):
+        # print("-------------------------------------")
+        # print(U[i])
+        # print(matrix)
+        matrix = multiply_matrices(U[i], matrix)
+    return matrix
+
 #test
 if __name__ == "__main__":
     parity_check=np.array([[1,1,0,1,0,0],[0,1,1,0,1,0],[1,1,1,0,0,1]])
     r = np.array([[1,1,0,1,1,0]])
     print(find_syndrome(parity_check,r))
+    print(random_permutation_matrix(6))
--- prange.py
+++ prange.py
@@ -9,16 +9,27 @@ def prange(s, H, t):
     H: parity check matrix
     t: error correction capability
     """
-    n = H.shape[1]
+    m,n = H.shape
     current_weight = math.inf
     while current_weight > t:
-        rand_permutation = np.random.randint(2,size=(n,n))
-        H_hat = cu.multiply_matrices(H, rand_permutation)
-        print(H_hat)
+        
+        permutation_chosen = False
+        while not permutation_chosen:
+            rand_permutation = cu.random_permutation_matrix(n)
+            H_hat = cu.multiply_matrices(H, rand_permutation)
+            #U son las transformaciones
+            U, H_hat = cu.gaussian_elimination(H_hat, start_column=n-m)
+            if np.array_equal(H_hat[:,n-m:], np.identity(m, dtype=int)):
+                # permutacion es valida si h_hat es full rank
+                permutation_chosen = True
+        
+        s_bar = cu.apply_transforms(U, s)
+        e_hat = np.zeros(n-m, dtype=int)
+        e_hat = np.hstack((e_hat, s_bar.transpose()[0]))
 
-
+        current_weight = np.sum(e_hat)
     
-
+    return cu.multiply_matrices(np.array([e_hat]), rand_permutation)
 
 
 
@@ -30,8 +41,24 @@ if __name__ == "__main__":
     H = np.array([[1,1,0,1,0,0],[0,1,1,0,1,0],[1,1,1,0,0,1]])
     r = np.array([[1,1,0,1,1,0]])
     s = cu.find_syndrome(H,r)
-    print(s)
 
-    rand_permutation = np.random.randint(2,size=(n,n))
-    H_hat = cu.multiply_matrices(H, rand_permutation)
-    print(H_hat)
+    e = prange(s,H,t)
+    computed_s = cu.find_syndrome(H, e)
+
+    # en teoria, H*e^t = s
+    print("Error vector: \n", e)
+    print("Syndrome: \n", computed_s)
+    print("actual syndrome: \n ", s)
+    print("Error weight: ", np.sum(e))
+
+    successes = 0
+    for i in range(1000):
+        e = prange(s,H,t)
+        computed_s = cu.find_syndrome(H, e)
+        if np.array_equal(computed_s, s):
+            successes += 1
+    
+    print("Successes: ", successes)
+    print("Success rate: ", successes/1000)
+
+
