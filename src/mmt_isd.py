d89265ef5b5c54ccabdd08810fa8b28572d2f1e2
juancamilobonet2 <juancamilo.bonet@gmail.com>
2024-03-13 19:28:07 -0500

    Started mmt implementation

---
+++ src/mmt_isd.py
@@ -0,0 +1,11 @@
+import math
+import code_utils as cu
+import time
+from sage.all import *
+
+def mmt_isd(H, s, w):
+    # Implementation of May-Meurer-Thomae ISD algorithm
+    # Input: Parity check matrix H (n-k x n), Syndrome s (length n-k), Natural number w.
+
+
+    pass
